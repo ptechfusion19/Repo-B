9e89af39c23abf9ae10667415bc8d5b872547b61
Ramzan <ramzanx0553@gmail.com>
2026-01-15 14:20:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Test JavaScript File for Folder 1
+console.log("Testing Folder 1 - JavaScript");
+
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to the test page.`;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Page loaded successfully");
+});
+
